fad59cabf7ef815f6d9d890aed99f8be831cc8b7
Shantanu Sinha <shantanusinha95@gmail.com>
2019-10-18 00:49:09 +0530

    Second commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,4 +6,5 @@ app_name = "myapp"
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('download_excel', views.download_excel, name='download_excel'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,23 +1,34 @@
 from django.shortcuts import render
 import openpyxl
+import os
+import io
+from django.http import HttpResponse
+# import xlsxwriter
+from xlsxwriter.workbook import Workbook
 
+
+
+def download_excel(request):
+    response = HttpResponse(content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+    response['Content-Disposition'] = "attachment; filename=result.xlsx"
+
+    wb = openpyxl.load_workbook("result.xlsx")
+    return response
 
 def index(request):
     if "GET" == request.method:
-        return render(request, 'myapp/index.html', {})
+        return render(request, 'myapp/index.html', {"upload_url": ""})
     else:
         excel_file = request.FILES["excel_file"]
-
         # you may put validations here to check extension or file size
 
         wb = openpyxl.load_workbook(excel_file)
 
         # getting all sheets
-        sheets = wb.sheetnames
-        print(sheets)
+        worksheet = wb.worksheets[0]
 
         # getting a particular sheet
-        worksheet = wb["Sheet 1"]
+        # worksheet = sheets[0]
         print(worksheet)
 
         # getting active sheet
@@ -28,7 +39,6 @@ def index(request):
         print(worksheet["A1"].value)
 
         excel_data = list()
-
 
         # iterating over the rows and
         # getting value from each cell in row
@@ -41,6 +51,7 @@ def index(request):
             cell_count = 1
             row_val = ""
             col_val = ""
+            row_data = list()
             for cell in row:
                 cell_count += 1
                 row_val = int(str(cell.row))
@@ -50,10 +61,15 @@ def index(request):
                 sum += int(str(cell.value))
             if(cell_count != 1) and sum != 0:
                 worksheet.cell(row=row_val, column=col_val).value = sum
+            excel_data.append(row_data)
 
-        wb.save("result.xlsx")
-
-        return render(request, 'myapp/index.html', {"excel_data":excel_data})
+        response = HttpResponse(content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+        response['Content-Disposition'] = "attachment; filename=result.xlsx"
+        wb.save(response)
+        return response
+        # wb.save("result.xlsx")
+        # wb.close()
+        # return render(request, 'myapp/index.html', {"excel_data":excel_data, "show_download": True})
 
 
 
